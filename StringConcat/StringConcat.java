57375935bb86d1aa488c344b7a53f258760f483a
nicgen <nic@genty.dev>
2025-08-18 12:43:49 +0200

    correct StringConcat

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -1,7 +1,7 @@
 public class StringConcat {
     public static String concat(String s1, String s2) {
         if (s1 == null) {
-            return s2
+            return s2;
         }
         if (s2 == null) {
             return s1;
