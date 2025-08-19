f055d4b2aacbb5ac9c220006ea2d570638e8d863
nicgen <nic@genty.dev>
2025-08-19 15:05:19 +0200

    corr Cat

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -2,18 +2,18 @@ import java.io.*;
 
 public class Cat {
     public static void cat(String[] args) throws IOException {
-        if (args.length > 0) {
-            try {
-                FileInputStream fstream = new FileInputStream(args[0]);
-                BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
-                String strLine;
-                while ((strLine = br.readLine()) != null) {
-                    System.out.println(strLine);
-                }
-                fstream.close();
-            } catch (Exception e) {
-                System.err.println("Error: " + e.getMessage());
+        if (args == null || args.length == 0) {
+            return; // nothing to do
+        }
+
+        try (InputStream in = new BufferedInputStream(new FileInputStream(args[0]))) {
+            OutputStream out = System.out;
+            byte[] buffer = new byte[8192];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
             }
+            out.flush();
         }
     }
 }
