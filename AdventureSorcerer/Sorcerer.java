e6b9166fd2b214376ba8ec262bf61c84863dbddb
nicgen <nic@genty.dev>
2025-08-26 15:57:16 +0200

    corr AdventureSorcerer

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,8 +1,29 @@
-public interface Sorcerer implements Healer {
-    private final int healCapacity();
+public class Sorcerer extends Character implements Healer {
+    private final int healCapacity;
 
-    public Sorcerer (String name, int maxHealth, int healCapacity){
+    public Sorcerer(String name, int maxHealth, int healCapacity) {
         super(name, maxHealth);
-        this.healCapacity =
-    };
+        this.healCapacity = healCapacity;
+    }
+
+    @Override
+    public int getHealCapacity() {
+        return healCapacity;
+    }
+
+    @Override
+    public void heal(Character target) {
+        if (target == null) return;
+        int healed = Math.min(target.getMaxHealth(), target.getCurrentHealth() + healCapacity);
+        // currentHealth is updated thanks to Character's protected visibility
+        target.currentHealth = healed;
+    }
+
+    @Override
+    public String toString() {
+        if (getCurrentHealth() == 0) {
+            return getName() + " is a dead sorcerer. So bad, it could heal " + healCapacity + " HP.";
+        }
+        return getName() + " is a sorcerer with " + getCurrentHealth() + " HP. It can heal " + healCapacity + " HP.";
+    }
 }
