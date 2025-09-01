e60133e4887446c2b189632e2b18307c24908976
nicgen <nic@genty.dev>
2025-09-01 11:04:43 +0200

    corr FormatDate

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -11,7 +11,7 @@ public class FormatDate {
             return null;
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(
-                "'Le' d MMMM 'de l''an' uuuu 'à' HH'h'mm'm et' ss's'",
+                "'Le' d MMM 'de l''an' uuuu 'à' HH'h'mm'm et' ss's'",
                 Locale.FRENCH
         );
         return dateTime.format(formatter);
